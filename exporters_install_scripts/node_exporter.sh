d1e3964e6edc66d1a39418193493dd8b375afe1e
root <root@localhost.localdomain>
2019-06-25 15:49:50 +0000

    node_exporter.sh  added

---
+++ exporters_install_scripts/node_exporter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+useradd -m -s /bin/bash monitoring
+mkdir -p /opt/node_exporter/textfile_directory
+chown root:monitoring /opt/node_exporter/textfile_directory
+chmod 770 /opt/node_exporter/textfile_directory
+
+wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0/node_exporter-0.16.0.linux-amd64.tar.gz -O /opt/node_exporter/node_exporter
+chown root:monitoring /opt/node_exporter/node_exporter
+chmod 750 /opt/node_exporter/node_exporter
+
+echo '[Unit]
+Description=Prometheus node_exporter
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/opt/node_exporter/node_exporter --collector.textfile.directory=/opt/node_exporter/textfile_directory
+User=monitoring
+Restart=always
+RestartSec=10
+StartLimitInterval=0
+
+[Install]
+WantedBy=multi-user.target
+' > /etc/systemd/system/node_exporter.service
+
+chmod 644 /etc/systemd/system/node_exporter.service
+
+systemctl daemon-reload
+systemctl enable node_exporter.service
+systemctl restart node_exporter.service
+
